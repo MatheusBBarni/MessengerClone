aec9e73360fa0c616140ad13bcebf585963b2aef
MatheusBBarni <brehm.matheus@hotmail.com>
2022-09-25 00:19:47 -0300

    first commit

---
+++ MessengerClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessengerClone
+//
+//  Created by Matheus Barni on 25/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
